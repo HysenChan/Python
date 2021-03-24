ec3593248e4bfeaf408439226def331933df1f1f
HysenChan <HysenChan@163.com>
2021-03-24 11:09:38 +0800

    Import file

---
+++ 2021/Project_0324/Import.py
@@ -0,0 +1,12 @@
+import sys
+
+print('Python import mode')
+print('命令行参数为：')
+for i in sys.argv:
+    print(i)
+print('\n python 路径为：', sys.path)
+
+from sys import argv, path
+
+print('Python import mode 2')
+print('path:', path)

b8e115383d8c3d73b08e746aca7c7ed9a7630127
HysenChan <HysenChan@163.com>
2021-03-25 18:35:35 +0800

    运算-成员运算符

--- 2021/Project_0325/Arithmetic.py
+++ 2021/Project_0325/Arithmetic.py
@@ -8,3 +8,18 @@ print(a, b)  # 60 13
 print(a << 2)  # 240 左移2位
 print(a >> 3)  # 7 右移2位
 
+
+# 成员运算符
+def judgeValueInList(value):
+    if value in list:
+        print("{} is in list!".format(value))
+    else:
+        print("{} isn't in list!".format(value))
+
+
+a = 10
+b = 1
+list = {1, 2, 3, 4, 5}
+
+judgeValueInList(a)
+judgeValueInList(b)
